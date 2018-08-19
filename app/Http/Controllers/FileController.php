e0a442b874ff39f774c4c5775f8636c8805db0d7
Milan Vasic <milan.vasic.su@gmail.com>
2018-08-19 09:27:20 +0200

    Implemented File API endpoint due to main requirement

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,8 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\File;
 
 class FileController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return File::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'file' => 'required'
+        ]);
+
+        $uploadedFile = $request->file('file');
+        $filename = time().$uploadedFile->getClientOriginalName();
+
+        Storage::disk('local')->putFileAs(
+          'files/'.$filename,
+          $uploadedFile,
+          $filename
+        );
+
+        $file = new File;
+        $file->original_name = $filename;
+
+        $file->save();
+
+        return response()->json([
+          'id' => $file->id
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        $file = File::find($id);
+        if (!empty($file)) {
+            Storage::delete($file->original_name);
+            $file->delete();
+            return response()->json(null, 204);
+        } else {
+            return response()->json('File not exists', 502);
+        }
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\File;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('files', 'FileController@index');
+Route::post('files','FileController@store');
+Route::delete('files/{product}', 'FileController@delete');
